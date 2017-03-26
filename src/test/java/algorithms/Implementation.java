329dc8aa7d6fea93cc5f351a5b5b1e4d542557ff
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-03-26 18:24:37 -0300

    +Implementation - apple and orange

--- src/test/java/algorithms/Implementation.java
+++ src/test/java/algorithms/Implementation.java
@@ -3,13 +3,14 @@ package algorithms;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Implementation {
 
     @Test
     public void gradingStudents() {
-        int[] grades = {80,96,18,73,78,60,60,15,45,15,10,5,46,87,33,60,14,71,65,2,5,97,0};
+        int[] grades = {80, 96, 18, 73, 78, 60, 60, 15, 45, 15, 10, 5, 46, 87, 33, 60, 14, 71, 65, 2, 5, 97, 0};
         List<Integer> gradeList = new ArrayList<>();
         for (int grade : grades) {
             if (grade < 38) {
@@ -18,7 +19,7 @@ public class Implementation {
                 gradeList.add(some(grade));
             }
         }
-        for(int grade : gradeList){
+        for (int grade : gradeList) {
             System.out.println(grade);
         }
     }
@@ -33,5 +34,46 @@ public class Implementation {
             count++;
         } while (++numero % 5 != 0 && count < 3);
         return count < 3 ? numero : numeroAux;
+    }
+
+    @Test
+    public void appleAndOrange() {
+        int s = 7;
+        int t = 11;
+        int a = 5;
+        int b = 15;
+        int m = 3;
+        int n = 2;
+        int[] apple = new int[m];
+        List<Integer> appleList = Arrays.asList(-2, 2, 1);
+
+        for (int i = 0; i < m; i++) {
+            apple[i] = appleList.get(i);
+        }
+
+        int[] orange = new int[n];
+        List<Integer> orangeList = Arrays.asList(5, -6);
+        for (int j = 0; j < n; j++) {
+            orange[j] = orangeList.get(j);
+        }
+
+        int appleCount = 0;
+        int orangeCount = 0;
+
+        for (int appleFall : apple) {
+            int place = a + appleFall;
+            if(place >= s && place <= t){
+                appleCount++;
+            }
+        }
+        for(int orangeFall : orange){
+            int place = b + orangeFall;
+            if(place >= s && place <= t){
+                orangeCount++;
+            }
+        }
+
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
     }
 }
