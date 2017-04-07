815e294e2cb8f3ce56b8fcac1c84e73453c54dfa
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-04-06 23:09:35 -0300

    +Strings - super reduced string

--- src/test/java/algorithms/Strings.java
+++ src/test/java/algorithms/Strings.java
@@ -22,4 +22,28 @@ public class Strings {
         String s = "saveChangesInTheEditor";
         System.out.println(s.split("[A-Z]").length);
     }
+
+    @Test
+    public void superReducedString() {
+        String teste = "aaabccddd";
+
+        boolean stop;
+        do {
+            String aux = "";
+            stop = true;
+            int limit = teste.length();
+            for (int i = 0; i < limit - 1; i++) {
+                if (teste.charAt(i) == teste.charAt(i + 1)) {
+                    teste = aux + teste.substring(i + 2, limit);
+                    stop = false;
+                    break;
+                } else {
+                    aux += teste.charAt(i);
+                }
+            }
+        } while (!stop);
+
+        System.out.println(!teste.isEmpty() ? teste : "Empty String");
+    }
+
 }
