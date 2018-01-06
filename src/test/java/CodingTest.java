aeddc53ebf6031a985a71fe8904dde079c505be2
snaplucas <lucas.chapola@gmail.com>
2018-01-06 21:42:52 -0200

    +Coding test: fixes

--- src/test/java/CodingTest.java
+++ src/test/java/CodingTest.java
@@ -1,21 +1,26 @@
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CodingTest {
 
     @Test
     public void foo() {
-        int[] myArray = new int[]{90, 10, 70, 30, 20};
-        System.out.println(minimumNumberOfTrips(90, myArray));
+        int[] myArray = new int[]{80, 10, 90, 20};
+
+        System.out.println(minimumNumberOfTrips(100, myArray));
+        System.out.println(minimumNumberOfTrips_2(100, myArray));
     }
 
     private List<Integer> greatestFlightRangeDrones(Integer numberOfRequiredDrones, List<Drone> drones, List<Integer> inMaintenanceDrones) {
-        return drones.stream()
-                .filter(x -> inMaintenanceDrones.stream().noneMatch(y -> y != x.id))
-                .sorted(Comparator.comparing(Drone::getFlightRange).reversed())
-                .limit(numberOfRequiredDrones)
+        return drones.stream() //
+                .filter(x -> inMaintenanceDrones.stream().noneMatch(y -> y != x.id)) //
+                .sorted(Comparator.comparing(Drone::getFlightRange).reversed()) //
+                .limit(numberOfRequiredDrones) //
                 .map(y -> y.id).collect(Collectors.toList());
     }
 
@@ -38,21 +43,39 @@ public class CodingTest {
     }
 
     private int minimumNumberOfTrips(int tripMaxWeight, int[] packagesWeight) {
-        List<Integer> lista = new ArrayList<>();
-        for (int i : packagesWeight) {
-            lista.add(i);
-        }
+        Arrays.sort(packagesWeight);
+
         int trips = 0;
+        int packages = 0;
+        int weight = 0;
 
-        lista.sort(Collections.reverseOrder());
-
-        for (int i = 0; i < lista.size(); i++) {
-            int item = lista.get(i);
-            Optional<Integer> pack = lista.stream().filter(x -> x + item <= tripMaxWeight).findFirst();
-            pack.ifPresent(lista::remove);
+        for (int i = packagesWeight.length; i > 0; i--) {
+            weight += packagesWeight[i - 1];
+            packages++;
+            if (weight > tripMaxWeight || packages > 1) {
+                trips++;
+                weight = 0;
+                packages = 0;
+            }
+        }
+        if (weight > 0) {
             trips++;
         }
 
+        return trips;
+    }
+
+    private int minimumNumberOfTrips_2(int tripMaxWeight, int[] packagesWeight) {
+        List<Integer> lista = Arrays.stream(packagesWeight).boxed().collect(Collectors.toList());
+        int trips = 0;
+
+        for (int i = 0; i < lista.size(); i++) {
+            int item = lista.get(i);
+            List<Integer> packs = lista.stream().filter(x -> x + item <= tripMaxWeight).collect(Collectors.toList());
+            Optional<Integer> max = packs.stream().reduce(Integer::max);
+            max.ifPresent(lista::remove);
+            trips++;
+        }
         return trips;
     }
 
